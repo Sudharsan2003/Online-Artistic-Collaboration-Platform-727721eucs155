96770ba29caa65020985ef9eb1d3b85f7f04b578
Sudharsan2003 <113884348+Sudharsan2003@users.noreply.github.com>
2023-07-28 12:33:16 +0530

    Delete BackEnd directory

--- BackEnd/online_1/src/main/java/com/example/demo/Repository/repository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.User;
-
-public interface repository  extends JpaRepository<User,Integer>{
-
-}
